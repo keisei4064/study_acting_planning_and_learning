8ddf3820eedb3c47fc7fbc8d2f203c6def10cb98
Keisei Tsubomoto <keisei20021415@gmail.com>
2026-06-05 11:34:30 +0900

    mapper らへんは明日にまわす

--- src/state_transition_system/binding.py
+++ src/state_transition_system/binding.py
@@ -1,5 +1,9 @@
 from __future__ import annotations
 import state_transition_system.state_variable_repr as svrepr
+from typing import Callable, TypeAlias
+
+
+TermMapper: TypeAlias = Callable[[svrepr.ObjectTerm], svrepr.ObjectTerm]
 
 
 def instantiate():
