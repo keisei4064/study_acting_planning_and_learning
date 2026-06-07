27e7fd998d15a670cd09edfb34fdc784e5f3ed7b
Keisei Tsubomoto <keisei20021415@gmail.com>
2026-06-07 19:12:09 +0900

    Ractive-Executionの実装

--- src/state_transition_system/plan_runner.py
+++ src/state_transition_system/plan_runner.py
@@ -1,34 +1,63 @@
 import state_transition_system.system as stssys
 import state_transition_system.problem as stsprob
 from typing import TypeVar, Callable, TypeAlias
-
+import enum
 
 StateT = TypeVar("StateT")
 DomainT = TypeVar("DomainT")
 
 
-StatePercepter: TypeAlias = Callable[[DomainT, StateT], StateT]
+StateObserver: TypeAlias = Callable[[DomainT], StateT]
+ActionPerformer: TypeAlias = Callable[[DomainT, stssys.Action[StateT]], None]
+
+
+class ExecutionResult(enum.Enum):
+    SUCCESS = 0
+    FAILURE = 1
 
 
 def run_plan(
     domain: DomainT,
-    s0: StateT,
     pi: stssys.Plan[StateT],
     goal: stsprob.GoalFormula[StateT],
-    percepter: StatePercepter[DomainT, StateT],
-) -> bool:
-    """Algorithm 2.1."""
-    s = s0
+    observer: StateObserver[DomainT, StateT],
+    performer: ActionPerformer[DomainT, StateT],
+) -> ExecutionResult:
+    """Algorithm 2.1. (p.16)"""
     while True:
-        s = percepter(domain, s)
+        s = observer(domain)
 
         if pi.length == 0:
-            return goal(s)
+            return ExecutionResult.SUCCESS if goal(s) else ExecutionResult.FAILURE
 
         a = pi.actions.pop(0)
         if not a.is_applicable(s):
-            return False
+            return ExecutionResult.FAILURE
 
-        s = a.transition(s)
-        if s is None:
-            return False
+        performer(domain, a)
+
+
+def reactive_execution(
+    domain: DomainT,
+    pi: stssys.Plan[StateT],
+    goal: stsprob.GoalFormula[StateT],
+    observer: StateObserver[DomainT, StateT],
+    performer: ActionPerformer[DomainT, StateT],
+) -> ExecutionResult:
+    """Algorithm 2.2. (p.26)"""
+    while True:
+        s = observer(domain)
+        if goal(s):
+            return ExecutionResult.SUCCESS
+
+        a: stssys.Action[StateT] | None = None
+        for i in range(pi.length, 0, -1):
+            suffix_plan = pi.suffix(i - 1)
+            expected_state = stssys.transition_by_plan(s, suffix_plan)
+            if expected_state is not None and goal(expected_state):
+                a = suffix_plan.actions[0]
+                break
+        if a is None:
+            return ExecutionResult.FAILURE
+
+        performer(domain, a)
